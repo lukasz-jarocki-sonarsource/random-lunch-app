06b201a2f70bee652c1fd49dc71510ece2ce940f
Steve Marion <steve.marion@sonarsource.com>
2022-10-03 12:14:33 +0200

    base commit of backend structure

---
+++ lunchapp/src/test/java/com/sonarsource/qubee/lunchapp/LunchappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonarsource.qubee.lunchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
