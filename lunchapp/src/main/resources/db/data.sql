7c4a8e9d6ca0688f10dad53972ffc16f568ba940
Steve Marion <steve.marion@sonarsource.com>
2022-10-04 17:08:00 +0200

    fix apostrophe

--- lunchapp/src/main/resources/db/data.sql
+++ lunchapp/src/main/resources/db/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO USER_PROFILE (NAME, RESTAURANTS)
-VALUES ('Hugo First', 'Il Vesuvio;Le Blandonnet;Café de l’Aviation;Mamasan'),
-       ('Percy Vere', 'Il Vesuvio;Le Blandonnet;Café de l’Aviation;Mamasan'),
-       ('A. Mused', 'Il Vesuvio;Le Blandonnet;Café de l’Aviation;Mamasan');
+VALUES ('Hugo First', 'Il Vesuvio;Le Blandonnet;Aviation;Mamasan'),
+       ('Percy Vere', 'Il Vesuvio;Le Blandonnet;Aviation;Mamasan'),
+       ('A. Mused', 'Il Vesuvio;Le Blandonnet;Aviation;Mamasan');
