77969a9b374480cce69c9e4fb15340b90cc5b37c
Steve Marion <steve.marion@sonarsource.com>
2022-10-04 09:51:33 +0200

    fix cron expression parsing. fix json param parsing for signup.

--- lunchapp/src/main/java/com/sonarsource/qubee/lunchapp/LunchMatchController.java
+++ lunchapp/src/main/java/com/sonarsource/qubee/lunchapp/LunchMatchController.java
@@ -5,6 +5,8 @@ import com.sonarsource.qubee.lunchapp.core.UserService;
 import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
@@ -12,11 +14,12 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +27,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("lunch")
 public class LunchMatchController {
+
+  Logger logger = LoggerFactory.getLogger(LunchMatchController.class);
 
   private final UserService userService;
 
@@ -33,7 +38,9 @@ public class LunchMatchController {
   }
 
   @PostMapping(path = "signup")
-  public void registerForLunch(HttpServletRequest request, @RequestParam("name") String signupName) {
+  public void registerForLunch(HttpServletRequest request, @RequestBody SignupForm signupForm) {
+    final String signupName = signupForm.name();
+    Assert.notNull(signupName, "name parameter must be present");
     userService.createUserProfile(signupName);
 
     PreAuthenticatedAuthenticationToken authentication = new PreAuthenticatedAuthenticationToken("", "");
@@ -46,11 +53,15 @@ public class LunchMatchController {
     session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, sc);
   }
 
-  @ExceptionHandler
+  @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   @ResponseBody
-  public String errorHandling() {
+  public String errorHandling(Exception e) {
+    logger.error("error while processing the query", e);
     return "You are wrong somehow.";
+  }
+
+  public record SignupForm(String name) {
   }
 
 
--- lunchapp/src/main/java/com/sonarsource/qubee/lunchapp/core/UserService.java
+++ lunchapp/src/main/java/com/sonarsource/qubee/lunchapp/core/UserService.java
@@ -1,15 +1,12 @@
 package com.sonarsource.qubee.lunchapp.core;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.support.CronExpression;
@@ -23,12 +20,11 @@ public class UserService {
 
   private final LocalTime lunchStart;
 
-  @Autowired
-  public UserService(UserDao userDao, @Value("match.making.cron") String lunchCron) {
+  public UserService(UserDao userDao, @Value("${match.making.cron}") String lunchCron) {
     this.userDao = userDao;
-    Instant next = CronExpression.parse(lunchCron).next(Instant.now());
+    LocalDateTime next = CronExpression.parse(lunchCron).next(LocalDateTime.now());
     Assert.notNull(next, lunchCron + " does not yield any execution time");
-    this.lunchStart = LocalTime.ofInstant(next.plus(1, ChronoUnit.MINUTES), ZoneId.systemDefault());
+    this.lunchStart = next.toLocalTime();
   }
 
   public Optional<UserDao.LunchGroup> findMatch(UserDao.UserProfile userProfile) {
